bfd930156cd6952cd8e27fa8d311c70b063e051d
Ashraful Islam <theashrafislam@gmail.com>
2025-02-01 00:41:12 +0600

    dausi ui installed done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Hello i am home page</h1>
+            <button className="btn btn-primary">Primary</button>
         </div>
     );
 };
